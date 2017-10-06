cb570b3d5cd2b6be84805f5bf293bcd1d9705f5b
Rodrigo Gonzalez <Rodrigo Gonzalez>
2017-10-06 00:52:15 -0400

    Add project files.

---
+++ WebAPI_NG_TokenbasedAuth.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebAPI_NG_TokenbasedAuth;
+using WebAPI_NG_TokenbasedAuth.Controllers;
+
+namespace WebAPI_NG_TokenbasedAuth.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
